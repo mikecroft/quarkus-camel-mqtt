cf9075dde32073da74ccb4dcf37abafb30f6b008
Mike Croft <backtotheoldhouse@gmail.com>
2020-02-19 15:45:39 +0000

    Add MicroShed + Testcontainers for later use

--- src/test/java/io/mikecroft/GreetingResourceTest.java
+++ src/test/java/io/mikecroft/GreetingResourceTest.java
@@ -2,13 +2,28 @@ package io.mikecroft;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.microshed.testing.jupiter.MicroShedTest;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import javax.annotation.PostConstruct;
+
+@MicroShedTest
 @QuarkusTest
 public class GreetingResourceTest {
 
+    @Container
+    public static GenericContainer<?> mqtt = new FixedHostPortGenericContainer<>("eclipse-mosquitto:1.6.8")
+            .withFixedExposedPort(1883,1883);
+
+    @PostConstruct
+    public void init(){
+        System.setProperty("camel.component.paho.broker-url",mqtt.getMappedPort(1883).toString());
+    }
     @Test
     public void testHelloEndpoint() {
         given()
